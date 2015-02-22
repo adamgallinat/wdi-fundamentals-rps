fa519bd90f4ec6b6ea76b82365851b2e7343c01d
Adam Gallinat <adamgallinat@gmail.com>
2015-02-22 15:58:07 -0500

    Rewrote getWinner(), commented out previous version below

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -36,12 +36,42 @@ function getComputerMove(move) {
 }
 
 function getWinner(playerMove,computerMove) {
+    if (playerMove === 'rock') && (computerMove === 'rock') {
+        return 'tie';
+    }
+    else if (playerMove === 'rock') && (computerMove === 'paper') {
+        return 'computer';
+    }
+    else if (playerMove === 'rock') && (computerMove === 'scissors') {
+        return 'player';
+    }
+    else if (playerMove === 'paper') && (computerMove === 'rock') {
+        return 'player';
+    }
+    else if (playerMove === 'paper') && (computerMove === 'paper') {
+        return 'tie';
+    }
+    else if (playerMove === 'paper') && (computerMove === 'scissors') {
+        return 'computer';
+    }
+    else if (playerMove === 'scissors') && (computerMove === 'rock') {
+        return 'computer';
+    }
+    else if (playerMove === 'scissors') && (computerMove === 'paper') {
+        return 'player';
+    }
+    else {
+        return 'tie';
+    }
+}
+
+/*function getWinner(playerMove,computerMove) {
     var winner;
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    /* YOUR CODE HERE */
-    if (playerMove == "rock") {
+    /* YOUR CODE HERE 
+    if (playerMove === "rock") {
         if (computerMove === "rock") {
             winner = "tie";
         }
@@ -75,7 +105,7 @@ function getWinner(playerMove,computerMove) {
         }
     }
     return winner;
-}
+}*/
 
 function playToFive() {
     console.log("Let's play Rock, Paper, Scissors");
